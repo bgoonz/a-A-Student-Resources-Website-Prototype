b35a0be57a84f17465115ff8a7b5d9874990b5c9
bryan <bryan.guner@gmail.com>
2020-12-03 12:45:56 -0500

    before I do more damage

--- PUBLIC/js/bstest.js
+++
@@ -1,6 +0,0 @@
-const thisIsNOTCollege = (testsRNOTCurved = cohortisNOTZeroSum = true);
-teamworkMakesTheDreamWork = () =>
-  thisIsNOTCollege && testsRNOTCurved && cohortisNOTZeroSum
-    ? "it will all be worth it one day"
-    : "you're not in app academy";
-teamworkMakesTheDreamWork(); //! ---> teamworkMakesTheDreamWork():  it will all be worth it one day
